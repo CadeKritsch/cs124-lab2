537a3b4a84f853fffc8a20fff58e39a0cc168af5
Cade Kritsch <73767994+CadeKritsch@users.noreply.github.com>
2022-04-20 13:32:06 -0700

    Update TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,34 +1,72 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 import TodoItem from "./TodoItem";
 import "./todo-list.css";
 
 const TodoList = (props) => {
+  // appropriate todo items are grabbed using the path to the appropriate list passed in via props
+  const [todoItems] = useCollectionData(props.todoItemsRef);
+  const [isRenameClicked, setIsRenameClicked] = useState(false);
+  const [updatedName, setUpdatedName] = useState("");
+  // console.log(todoItems);
   return (
     <>
       <div className="list-container">
+        <div className="list-header">
+          <div tabindex="0" className="list-name">{props.listName}</div>
+          {isRenameClicked ? (
+            <>
+              <input
+                type="text"
+                name="updated-name"
+                id="updated-name"
+                value={updatedName}
+                onChange={(e) => {
+                  setUpdatedName(e.target.value);
+                }}
+              />
+              <button
+                onClick={() => {
+                  props.onListNameChange(props.listId, updatedName);
+                  setIsRenameClicked(false);
+                }}
+              >
+                Rename
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => {
+                setIsRenameClicked((prevState) => !prevState);
+              }}
+            >
+              Rename
+            </button>
+          )}
+        </div>
         {props.isCompletedShown
-          ? props.data.map((item) => {
+          ? todoItems?.map((item) => {
               return (
                 <>
                   <TodoItem
                     key={item.id}
                     name={item.itemName}
                     id={item.id}
-                    status={item.itemStatus}
+                    isCompleted={item.isCompleted}
                     onItemChange={props.onItemChange}
                   />
                 </>
               );
             })
-          : props.data.map((item) => {
-              return item.itemStatus === false ? (
+          : todoItems?.map((item) => {
+              return item.isCompleted === false ? (
                 <TodoItem
                   key={item.id}
                   name={item.itemName}
                   id={item.id}
-                  status={item.itemStatus}
+                  isCompleted={item.isCompleted}
                   onItemChange={props.onItemChange}
                 />
               ) : null;
@@ -39,9 +77,12 @@ const TodoList = (props) => {
 };
 
 TodoList.propTypes = {
-  data: PropTypes.array.isRequired,
   onItemChange: PropTypes.func,
+  onItemAdd: PropTypes.func,
   isCompletedShown: PropTypes.bool,
+  listName: PropTypes.string,
+  listId: PropTypes.string,
+  todoItemsRef: PropTypes.object,
 };
 
 export default TodoList;
